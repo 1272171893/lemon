2800ad1bafcbed37bb99a4552737ce3681076d04
1272171893 <1272171893@qq.com>
2021-03-02 23:50:07 +0800

    install element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <el-button type="primary">主要按钮</el-button>
+  <el-link :underline="false">无下划线</el-link>
 </template>
 
 <script setup>
