fcab39510040a3ea80a84e2ed80ae40878ba19b4
1272171893 <1272171893@qq.com>
2021-03-04 17:17:14 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
   <el-button type="primary">主要按钮</el-button>
-  <el-link :underline="false">无下划线</el-link>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
