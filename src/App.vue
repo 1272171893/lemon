541e8d55772c7b51be7628ef9c7b8173cfed1e57
1272171893 <1272171893@qq.com>
2021-03-04 18:14:40 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,13 @@
 <script setup>
 </script>
 <style>
+body{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin: 0;
-  padding: 0;
 }
 </style>
