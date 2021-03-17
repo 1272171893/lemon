2fbc56bd2a35aba6c8544e0f9149164c2041fe27
1272171893 <1272171893@qq.com>
2021-03-17 17:36:00 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-import App from "./App.vue";
-const app = createApp(App);
+import 'normalize.css';
+import Home from "./Home.vue";
+const app = createApp(Home);
 app.use(ElementPlus);
 app.mount("#app");
