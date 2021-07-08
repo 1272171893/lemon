192f0c0c268844e740a75f342421b77e79205253
小鱼人 <1272171893@qq.com>
2021-07-08 22:41:43 +0800

    修改目录结构使其分类合理

---
+++ src/library/configs/index.ts
@@ -0,0 +1,120 @@
+import Border from "./Basic/Border";
+import Button from "./Basic/Button";
+import Color from "./Basic/Color";
+import Container from "./Basic/Container";
+import Icon from "./Basic/Icon";
+import Layout from "./Basic/Layout";
+import Link from "./Basic/Link";
+import Typography from "./Basic/Typography";
+import Avatar from "./Data/Avatar";
+import Badge from "./Data/Badge";
+import Empty from "./Data/Empty";
+import Pagination from "./Data/Pagination";
+import Progress from "./Data/Progress";
+import Skeleton from "./Data/Skeleton";
+import Table from "./Data/Table";
+import Tag from "./Data/Tag";
+import Tree from "./Data/Tree";
+import Cascader from "./Form/Cascader";
+import Checkbox from "./Form/Checkbox";
+import ColorPicker from "./Form/ColorPicker";
+import DatePicker from "./Form/DatePicker";
+import DateTimePicker from "./Form/DateTimePicker";
+import Form from "./Form/Form";
+import Input from "./Form/Input";
+import InputNumber from "./Form/InputNumber";
+import Radio from "./Form/Radio";
+import Rate from "./Form/Rate";
+import Select from "./Form/Select";
+import Slider from "./Form/Slider";
+import Switch from "./Form/Switch";
+import TimePicker from "./Form/TimePicker";
+import Transfer from "./Form/Transfer";
+import Upload from "./Form/Upload";
+import Breadcrumb from "./Navigation/Breadcrumb";
+import Dropdown from "./Navigation/Dropdown";
+import NavMenu from "./Navigation/NavMenu";
+import PageHeader from "./Navigation/PageHeader";
+import Steps from "./Navigation/Steps";
+import Tabs from "./Navigation/Tabs";
+import Alert from "./Notice/Alert";
+import Loading from "./Notice/Loading";
+import Message from "./Notice/Message";
+import MessageBox from "./Notice/MessageBox";
+import Notification from "./Notice/Notification";
+import Backtop from "./Others/Backtop";
+import Calendar from "./Others/Calendar";
+import Card from "./Others/Card";
+import Carousel from "./Others/Carousel";
+import Collapse from "./Others/Collapse";
+import Dialog from "./Others/Dialog";
+import Divider from "./Others/Divider";
+import Drawer from "./Others/Drawer";
+import Image from "./Others/Image";
+import InfiniteScroll from "./Others/InfiniteScroll";
+import Popconfirm from "./Others/Popconfirm";
+import Popover from "./Others/Popover";
+import Timeline from "./Others/Timeline";
+import Tooltip from "./Others/Tooltip";
+import { Iconfigs } from "../index";
+const configs: Iconfigs = {
+  Border,
+  Button,
+  Color,
+  Container,
+  Icon,
+  Layout,
+  Link,
+  Typography,
+  Avatar,
+  Badge,
+  Empty,
+  Pagination,
+  Progress,
+  Skeleton,
+  Table,
+  Tag,
+  Tree,
+  Cascader,
+  Checkbox,
+  ColorPicker,
+  DatePicker,
+  DateTimePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Rate,
+  Select,
+  Slider,
+  Switch,
+  TimePicker,
+  Transfer,
+  Upload,
+  Breadcrumb,
+  Dropdown,
+  NavMenu,
+  PageHeader,
+  Steps,
+  Tabs,
+  Alert,
+  Loading,
+  Message,
+  MessageBox,
+  Notification,
+  Backtop,
+  Calendar,
+  Card,
+  Carousel,
+  Collapse,
+  Dialog,
+  Divider,
+  Drawer,
+  Image,
+  InfiniteScroll,
+  Popconfirm,
+  Popover,
+  Timeline,
+  Tooltip,
+};
+export default configs;
